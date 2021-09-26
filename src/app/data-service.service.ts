7073ade1941359429fe8c10c36eee671c51a6ceb
shobhitsrivastava-ds <shobhitsrm18th@gmail.com>
2021-09-26 14:24:25 +0530

    Added new files

---
+++ src/app/data-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  _url= ''
+  constructor(private _http: HttpClient) { }
+
+  enroll(data: string[]) {
+    console.log("Printing the data",data);
+    return(this._http.post<any>(this._url, data));
+  }
+
+}
